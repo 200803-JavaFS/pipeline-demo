c59f4fed778523a8ff43e8a486eb5a6dcc371a78
gattietime <gattietime@gmail.com>
2020-12-15 11:25:41 -0500

    new test

--- src/main/java/com/revature/controllers/HelloServlet.java
+++ src/main/java/com/revature/controllers/HelloServlet.java
@@ -8,8 +8,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class HelloServlet extends HttpServlet{
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+
+public class HelloServlet extends HttpServlet{
+	
+	private static final Logger log = LogManager.getLogger(HelloServlet.class);
 	private static final long serialVersionUID = 1L;
 
 	public HelloServlet() {
@@ -18,6 +23,7 @@ public class HelloServlet extends HttpServlet{
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		log.warn("Someone is saying hi.");
 		PrintWriter pw = res.getWriter();
 		pw.print("<h1> Hello from your doGet method; I'm on Jenkins</h1>");
 	}
